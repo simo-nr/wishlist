b9f8d9676a1335d602215c24a9207a06685cd9a5
simonrolly <simonrolly@outlook.com>
2024-02-01 22:10:30 +0100

    changed password

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     # SQLALCHEMY_DATABASE_URI = 'Simon1122.mysql.pythonanywhere-services.com'
     # SQLALCHEMY_DATABASE_URI = 'mysql://Simon1122:mydbpw2002(something)@Simon1122.mysql.pythonanywhere-services.com/Simon1122$default'
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password123@localhost/wishlist'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Simon1122:mydbpw2002@Simon1122.mysql.pythonanywhere-services.com/wishlist'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Simon1122:password123@Simon1122.mysql.pythonanywhere-services.com/wishlist'
     
     SECRET_KEY = 'hellothisisasecretkey' # TODO: set proper secret key
 
