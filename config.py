139ea6ecbc44412fd76ef50ff48411d28d0e2909
simonrolly <simonrolly@outlook.com>
2024-02-01 21:50:26 +0100

    changes to mysql db

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'Simon1122.mysql.pythonanywhere-services.com'
-    SQLALCHEMY_DATABASE_URI = 'mysql://Simon1122:mydbpw2002(something)@Simon1122.mysql.pythonanywhere-services.com/Simon1122$default'
+    # SQLALCHEMY_DATABASE_URI = 'mysql://Simon1122:mydbpw2002(something)@Simon1122.mysql.pythonanywhere-services.com/Simon1122$default'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password123@localhost/wishlist'
+    
     SECRET_KEY = 'hellothisisasecretkey' # TODO: set proper secret key
 
--- db_startup.py
+++ db_startup.py
@@ -1,9 +1,25 @@
-from app import app, db
+import mysql.connector
+# from app import app, db
 
-@app.cli.command()
-def initdb():
-    with app.app_context():
-        db.create_all()
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="password123",
+    auth_plugin='mysql_native_password'
+)
 
-if __name__ == "__main__":
-    app.run()
+my_cursor = mydb.cursor()
+# commented out to not accidently create another db
+# my_cursor.execute("CREATE DATABASE wishlist")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+
+# @app.cli.command()
+# def initdb():
+#     with app.app_context():
+#         db.create_all()
+
+# if __name__ == "__main__":
+#     app.run()
