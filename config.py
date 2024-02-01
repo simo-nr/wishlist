1e017e1c39ae8bf3136fc66d573a300ceb57bdbc
simonrolly <simonrolly@outlook.com>
2024-02-01 18:18:16 +0100

    changed URI and added password

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'Simon1122.mysql.pythonanywhere-services.com'
+    # SQLALCHEMY_DATABASE_URI = 'Simon1122.mysql.pythonanywhere-services.com'
+    SQLALCHEMY_DATABASE_URI = 'mysql://Simon1122:mydbpw2002(something)@Simon1122.mysql.pythonanywhere-services.com/Simon1122$default'
     SECRET_KEY = 'hellothisisasecretkey' # TODO: set proper secret key
 
